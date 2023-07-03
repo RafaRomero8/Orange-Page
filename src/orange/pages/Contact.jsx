1e526a66552187d7d44304848105e01e49c67646
Rafael Romero <RafaRomerox@gmail.com>
2023-07-03 16:48:05 -0600

    email

--- src/orange/pages/Contact.jsx
+++ src/orange/pages/Contact.jsx
@@ -1,138 +1,41 @@
 
-import  { useRef, useState } from 'react';
-import emailjs from '@emailjs/browser';
-import { Box, Button, TextField } from '@mui/material';
-import { Container, Grid } from "@mui/material"
-
-//console.log(import.meta.env.VITE_SERVICE_ID)
+import  { Fragment, useRef, useState } from 'react';
+//import emailjs from '@emailjs/browser';
+//import { Box, Button, TextField } from '@mui/material';
+//import { Container, Grid } from "@mui/material"
+//import { useForm } from 'react-hook-form';
+ 
 
 export const Contact = () => {
+  //const {register,errors,handleSubmit} = useForm();
 
-  const service = import.meta.env.VITE_SERVICE_ID
-  const template = import.meta.env.VITE_TEMPLATE_ID
-  const public_key = import.meta.env.VITE_PUBLIC_KEY
-
-  const form = useRef();
-  const [email, setEmail] = useState("");
-  const [error, setError] = useState({
-    error:false,
-    message:" ",
-  });
+  // const onSubmit = (data) =>{
+  //   console.log(data)
+  // }
 
 
-  const validateEmail = (email) =>{
-    const regex =  /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i;
-    return regex.test(email);
-  };
-
-  const sendEmail = (e) => {
-    e.preventDefault();
-
-    if(validateEmail(email)){
-      setError({
-        error:false,
-        message:"",
-      })
-      console.log("Email correcto");
-    }else{
-      setError({
-        error:true,
-        message:"Email incorrecto",
-      })
-      console.log("Email incorrecto");
-    }
-
-    emailjs.sendForm(service,template,
-     form.current, public_key)
-      .then((result) => {
-          console.log(result.text);
-          alert("Mensaje Enviado con Exito")
-          form.current.reset() //current melimpia el formulario
-      }, (error) => {
-          console.log(error.text);
-      });
-  };
   return (
     <>
-    
-     <h1>Contact</h1>
-    
-     <Container>  
-     
-      {/* <Box  component="form" ref="form" onSubmit={sendEmail}> */}
-      <form ref={form} onSubmit={sendEmail} className=''>
-      <TextField
-          name='user_name'
-          label="Name"
-          type="text"
-          variant='outlined'
-          fullWidth
-          error={true}
-          sx={{mb:2 }}
-        />
-          <TextField
-          name='user_phone'
-          label="Telefono"
-          type="text"
-          variant='outlined'
-          fullWidth
-          error={true}
-          sx={{mb:2 }}
-        />
-          <TextField
-          name='user_company'
-          label="Empresa"
-          type="text"
-          variant='outlined'
-          fullWidth
-          error={true}
-          sx={{mb:2 }}
-        />
-          <TextField
-          name='user_email'
-          label="Email"
-          type="email"
-          variant='outlined'
-          fullWidth
-          required
-          error={error.error}
-          helperText={error.message}
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          
-          sx={{mb:2 }}
-        />
-           <TextField
-          name='message'
-          label="Mensaje"
-          type="textarea"
-          variant='outlined'
-          fullWidth
-          error={true}
-          sx={{mb:2 }}
-          
-        />
-      
-        <Button 
-           type="submit"  
-           value="Send"
-          variant="contained"
-          sx={{mb:2 }}
-          >
-            Send
-        </Button>
-        </form>
-      {/* </Box> */}
-   
+      <Fragment>
+        <h1>contact</h1>
 
-    {/* <form ref={form} onSubmit={sendEmail} className=''>
-    <Grid xs={12}> 
-      
-        </Grid>
-      </form> */}
+        {/* <form onSubmit={handleSubmit(onSubmit)}>
+       <input
+          name='titulo'
+          ref={
+              register({
+                    required:{value:true,
+                     message:'Titulo obligatorio'}
+              })
+           }
+       
+       />
+
+       <button>Enviar</button>
+
+        </form> */}
+      </Fragment>
     
-    </Container>
-  
     </>
     
   )
