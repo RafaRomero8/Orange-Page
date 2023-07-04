524cd6a8c4019c251166c8d793c241f979599eb2
Rafael Romero <RafaRomerox@gmail.com>
2023-07-03 20:37:58 -0600

    use formik

--- src/orange/pages/Contact.jsx
+++ src/orange/pages/Contact.jsx
@@ -1,23 +1,84 @@
 
-import  { Fragment, useRef, useState } from 'react';
+import { Formik,useFormik } from 'formik';
+import  {useRef, useState } from 'react';
 //import emailjs from '@emailjs/browser';
 //import { Box, Button, TextField } from '@mui/material';
 //import { Container, Grid } from "@mui/material"
-//import { useForm } from 'react-hook-form';
- 
+import * as Yup from 'yup'
+//ir a yup para checar lasvalidaciones
+
+const schema = Yup.object().shape({
+  email:Yup.string().email('Email invalido').required(),
+  password:Yup.string().min(2).required(),
+
+})
+
 
 export const Contact = () => {
   //const {register,errors,handleSubmit} = useForm();
 
-  // const onSubmit = (data) =>{
-  //   console.log(data)
-  // }
 
+
+  const {handleSubmit,handleChange,handleReset,errors,values} = useFormik(
+    {
+      initialValues:{
+        email:'',
+        password:'' 
+      },
+      onSubmit: (values,{resetForm})=>{
+        resetForm();
+        console.log(values);
+       
+      },
+      validationSchema:schema,
+    })
+
+  //   const  handleReset=(e) =>{
+  //     e.target.reset();
+
+
+  //   } 
 
   return (
     <>
-      <Fragment>
+     
         <h1>contact</h1>
+        <form onSubmit={handleSubmit} onReset={handleReset}>
+          <input
+              type='email'
+              name='email'
+              value={values.email}
+              onChange={handleChange}
+          />
+          <input
+              type='password'
+              name='password'
+              value={values.password}
+              onChange={handleChange}
+              
+          />
+
+
+        <button type={'submit'}>Enviar</button>
+        <br/>
+        {errors.email && <span>invalido</span>}
+        {errors.password && <span>pass ivalido</span>}
+
+
+        </form>
+
+      {/* <Formik
+       onSubmit={() =>{
+          console.log('Form enviado')
+       }}
+      
+      >
+        {()=>  (
+         
+
+        )}
+      </Formik> */}
+
 
         {/* <form onSubmit={handleSubmit(onSubmit)}>
        <input
@@ -34,7 +95,7 @@ export const Contact = () => {
        <button>Enviar</button>
 
         </form> */}
-      </Fragment>
+  
     
     </>
     
