37ec307b8f37eabd8535c77348c09bba806bdfd8
Rafael Romero <RafaRomerox@gmail.com>
2023-07-06 18:56:56 -0600

    form with react-hook-form

--- src/orange/pages/Contact.jsx
+++ src/orange/pages/Contact.jsx
@@ -1,24 +1,14 @@
-
-import  { useRef, useState } from 'react';
+import { useEffect, useRef, useState } from 'react'
+import { useForm } from 'react-hook-form'
 import emailjs from '@emailjs/browser';
-import { Box, Button, TextField } from '@mui/material';
-import { Container, Grid } from "@mui/material"
-import * as Yup from 'yup'
-//console.log(import.meta.env.VITE_SERVICE_ID)
+//import ButtonsResult from "./ButtonsResult";
 
 export const Contact = () => {
+
 
   const service = import.meta.env.VITE_SERVICE_ID
   const template = import.meta.env.VITE_TEMPLATE_ID
   const public_key = import.meta.env.VITE_PUBLIC_KEY
-
-  const schema = Yup.object().shape({
-    //email:Yup.string().email('Email invalido').required(),
-    password:Yup.string().min(2).required(),
-  
-  })
-  
-  
 
   const form = useRef();
   const [email, setEmail] = useState("");
@@ -26,174 +16,127 @@ export const Contact = () => {
   const [interprise,setInterprise] = useState("");
   const [phone,setPhone] = useState("");
   const [name,setName] = useState("");
-  
-  const [error, setError] = useState({
-    error:false,
-    message:" ",
-  });
 
 
-  const [error_name, setError_name] = useState({
-    error_name:false,
-    message:" ",
-  });
-
-  //const [value, setValue] = useState('');
-
-  const validateEmail = (email) =>{
-    //const regex = /[^@ \t\r\n]+@[^@ \t\r\n]+\.[^@ \t\r\n]+/
-    const regex = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i;
-    return regex.test(email);
-  };
-
-  const validateName = () =>{
-       
-          schema
-    //return regex.test(email);
-  };
-
-
-  const sendEmail = (e) => {
-    e.preventDefault();
-    //setEmail('');
-    setMessage('');
-    setInterprise('');
-    setPhone('');
-    
-
-  const  send_email = ()=>{
-    emailjs.sendForm(service,template,
-      form.current, public_key)
-       .then((result) => {
-           console.log(result.text);
-           alert("Mensaje Enviado con Exito")
-         // form.current.reset() //current melimpia el formulario
-       }, (error) => {
-           console.log(error.text);
-       });
+  const { register, handleSubmit, formState: { errors }, reset,watch } = useForm() 
+  const customSubmit = () => {
+      //console.log(data)
+      //register('');
+      emailjs.sendForm(service,template,
+        form.current, public_key)
+         .then((result) => {
+          reset(
+            {
+              user_name:"",
+              user_phone:"",
+              user_company:"",
+              user_email:"",
+              message: "",
+            }
+          )
+             console.log(result.text);
+             //alert("Mensaje Enviado con Exito")
+           // form.current.reset() //current melimpia el formulario
+         }, (error) => {
+             console.log(error.text);
+         });
+         
+      //alert("¡Validación exitosa!")
   }
 
-   
-    if(validateEmail(email) && validateName()){
-      setError_name({
-        error_name:false,
-        message:"",
-      })
-      setError({
-        error:false,
-        message:"",   
-      })
-       //send_email()
-       setName('')
-       setEmail(''); 
-       console.log("Email correcto");
-    }else{
-      setError({
-        error:true,
-        message:"Email incorrecto",
-      })
-      setError_name({
-        error_name:true,
-        message:"Ingrese almenos 2 caracteres",
-      })
-      console.log("Email incorrecto");
-    }
-      //e.target.reset();
-  };
+  const [colorInput, setColorInput] = useState('#fff7f7')
+  useEffect( ()=> {
+      let words = watch('prueba')
+      if(words === 'react'){setColorInput('#614ad3')}
+  } )
+  
   return (
     <>
-    
-     <h1>Contact</h1>
-    
-     <Container>  
-     
-      {/* <Box  component="form" ref="form" onSubmit={sendEmail}> */}
-      <form ref={form} onSubmit={sendEmail} className=''>
-      <TextField
-          name='user_name'
-          label="Name"
-          type="text"
-          variant='outlined'
-          
-          color="warning"
-          fullWidth
-          //required='min'
-          error={error_name.error_name}
-          helperText={error_name.message}
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-          sx={{mb:2 }}
-        />
-          <TextField
-          name='user_phone'
-          label="Telefono"
-          type="text"
-          variant='outlined'
-          fullWidth
-          inputProps={{ inputMode: 'numeric', pattern: '[0-9]*' }}
-         // error={error.error}
-          value={phone}
-          onChange={(e) => setPhone(e.target.value)}
-          sx={{mb:2 }}
-        />
-          <TextField
-          name='user_company'
-          label="Empresa"
-          type="text"
-          variant='outlined'
-          fullWidth
-          //error={error.error}
-          value={interprise}
-          onChange={(e) => setInterprise(e.target.value)}
-          sx={{mb:2 }}
-        />
-          <TextField
-          name='user_email'
-          label="Email"
-          type="email"
-          variant='outlined'
-          fullWidth
-          required //poner required si el campo es obligatrio OJO
-          error={error.error}
-          helperText={error.message}
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          sx={{mb:2 }}
-        />
-           <TextField
-          name='message'
-          label="Mensaje"
-          type="textarea"
-          variant='outlined'
-          fullWidth
-          //error={error.error}
-          value={message}
-          onChange={(e) => setMessage(e.target.value)}
-          sx={{mb:2 }}
-          
-        />
-      
-        <Button 
-           type="submit"  
-           value="Send"
-          variant="contained"
-          sx={{mb:2 }}
-          >
-            Send
-        </Button>
-        </form>
-      {/* </Box> */}
-   
+        <h2>Form Validation</h2>
+        <form ref={form} onSubmit={ handleSubmit(customSubmit) } className='form-react'>
+            <div className='form-control'>
+                <label>Name</label>
+                <input  type="text" {...register('user_name',{ 
+                    required:true,
+                    minLength:5
+                })} />
+                {errors.name?.type === 'required' && <small className='fail'>El campo no puede estar vacío</small>}
+                {errors.name?.type === 'minLength' && <small className='fail'>El minimo de caracteres es 5</small>}
+                
+            </div>
+            {/* <div className='form-control'>
+                <label>Age</label>
+                <input type="number" {...register('age', {
+                    required:true,
+                    min:10,
+                    max:100
+                })} />
+                {errors.age?.type === 'required' && <small className='fail'>El campo no puede estar vacío</small>}
+                {errors.age?.type === 'min' && <small className='fail'>La edad mínima es de 10 años</small>}
+                {errors.age?.type === 'max' && <small className='fail'>La edad máxima es de 100 años</small>}
+            </div> */}
+            <div className='form-control'>
+                <label>Telefono</label>
+                <input  type="text" {...register('user_phone', {
+                    required: {
+                        value:true,
+                        message:"Custom Message: Ingrese los datos"
+                    }
+                })} />
+                {/* {errors.country && <small className='fail'>{errors.country.message}</small>} */}
+            </div>
+            <div className='form-control'>
+                <label>Empresa</label>
+                <input  type="text" {...register('user_company', {
+                    required: {
+                        value:true,
+                        message:"Custom Message: Ingrese los datos"
+                    }
+                })} />
+                {/* {errors.country && <small className='fail'>{errors.country.message}</small>} */}
+            </div>
+            <div className='form-control'>
+                <label>email</label>
+                <input  type="email" {...register('user_email', {
+                    required: {
+                        value:true,
+                        message:"Custom Message: Ingrese los datos"
+                    },
+                    pattern:/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i
+                })} />
+                {/* {errors.country && <small className='fail'>{errors.country.message}</small>} */}
+            </div>
 
-    {/* <form ref={form} onSubmit={sendEmail} className=''>
-    <Grid xs={12}> 
-      
-        </Grid>
-      </form> */}
-    
-    </Container>
-  
+            <div className='form-control'>
+                <label>Mensaje</label>
+                <input  type="text" {...register('message', {
+                    required: {
+                        value:true,
+                        message:"Custom Message: Ingrese los datos"
+                    }
+                })} />
+                {/* {errors.country && <small className='fail'>{errors.country.message}</small>} */}
+            </div>
+             {/* <div className='form-control'>
+                 <input {...register('prueba')} style={{ backgroundColor:colorInput }} />
+             </div>    */}
+
+
+                 {/* <ButtonsResult type='submit' {...{ reset }} /> */}
+           
+             {/* <button   
+               type="submit"
+                 
+           value="Send"
+        onClick={() => {
+          customSubmit()
+          
+        }}
+         >Send</button>  */}
+         <button type={'submit'} >Enviar</button>
+        </form>
     </>
-    
   )
 }
+
+//export default Contact
